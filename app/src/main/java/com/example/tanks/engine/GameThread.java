959895e1e1d1fb3b071f8d7565b6c0feb89f10ae
Marcin <markowitek@gmail.com>
2017-10-23 21:53:06 +0200

    create Main Game Thread with tests...

---
+++ app/src/main/java/com/example/tanks/engine/GameThread.java
@@ -0,0 +1,73 @@
+package com.example.tanks.engine;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class GameThread implements Runnable {
+
+    private final int FPS;
+    private long startTime;
+    private final SurfaceHolder surfaceHolder;
+    private SurfaceView surfaceView;
+    public static Canvas canvas;
+
+    public GameThread(SurfaceHolder surfaceHolder, SurfaceView surfaceView) {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.surfaceView = surfaceView;
+        this.FPS = 30;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                startTime = System.nanoTime();
+                updateUI();
+                waitToMakeCorrectFPSNumber();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main game thread interrupted");
+        }
+    }
+
+    private void waitToMakeCorrectFPSNumber() throws InterruptedException {
+        long targetTime = 1000 / FPS;
+        long timeMillis = (System.nanoTime() - startTime) / 1000000;
+        long waitTime = targetTime - timeMillis;
+        Thread.sleep(waitTime);
+    }
+
+    private void updateUI() {
+        canvas = null;
+        try {
+            lockCanvasAndUpdateUI();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            unlockCanvasAndPost();
+        }
+    }
+
+    private void unlockCanvasAndPost() {
+        if (canvas != null) {
+            try {
+                surfaceHolder.unlockCanvasAndPost(canvas);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void lockCanvasAndUpdateUI() {
+        canvas = this.surfaceHolder.lockCanvas();
+        updateAndDrawOnUI();
+    }
+
+    private void updateAndDrawOnUI() {
+        synchronized (surfaceHolder) {
+            this.surfaceView.draw(canvas);
+        }
+    }
+}
---
+++ app/src/test/java/com/example/tanks/engine/GameThreadTest.java
@@ -0,0 +1,91 @@
+package com.example.tanks.engine;
+
+import android.graphics.Canvas;
+import android.support.annotation.NonNull;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Marcin on 23.10.2017.
+ */
+public class GameThreadTest {
+
+    @Mock
+    SurfaceView surfaceView;
+
+    @Mock
+    SurfaceHolder surfaceHolder;
+
+    @Mock
+    Canvas canvas;
+
+    @InjectMocks
+    GameThread testedClass;
+
+    @BeforeMethod
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        when(surfaceHolder.lockCanvas()).thenReturn(canvas);
+        testedClass = new GameThread(surfaceHolder, surfaceView);
+    }
+
+    @Test
+    public void testIfStarted_ThenThreadIsRefreshingUI() {
+        Thread gameThread = givenStartedGameThread();
+        whenWaitForASecondAndInterruptGameThread(gameThread);
+        thenThereWasSomeUI_UpdatesRecorded();
+    }
+
+    @Test
+    public void testIfInterrupted_ThenThreadIsNoMoreRefreshingUI() {
+        Thread gameThread = givenStartedGameThread();
+        whenWaitForASecondAndInterruptGameThread(gameThread);
+        thenThereWasSomeUI_UpdatesRecorded();
+        whenWaitingForSomeTime(100);
+        reset(surfaceView,surfaceHolder);
+        whenWaitingForSomeTime(2000);
+        verifyZeroInteractions(surfaceHolder, surfaceView);
+    }
+
+    private void whenWaitingForSomeTime(int milis) {
+        try {
+            Thread.sleep(milis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void thenThereWasSomeUI_UpdatesRecorded() {
+        verify(surfaceHolder, atLeastOnce()).lockCanvas();
+        verify(surfaceView, atLeastOnce()).draw(canvas);
+    }
+
+    private void whenWaitForASecondAndInterruptGameThread(Thread thread) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread.interrupt();
+    }
+
+    @NonNull
+    private Thread givenStartedGameThread() {
+        testedClass = new GameThread(surfaceHolder, surfaceView);
+        Thread thread = new Thread(testedClass);
+        thread.start();
+        return thread;
+    }
+}
